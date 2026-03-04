a8149e537338e8ceb5f9a23f52608db9a13b6025
Danae Alejandra Cortes García <alejandradanae14@gmail.com>
2026-03-04 14:33:54 -0600

    Update run_CT.sh

--- run_CT.sh
+++ run_CT.sh
@@ -1,7 +1,7 @@
 
 python ~/Single_cell_pre/Celltypi_anno.py \
   --base /STORAGE/csbig/sc_ADers/celltypist/unassigned_only_minimal \
-  --model "/STORAGE/csbig/sc_ADers/Adult_Human_PrefrontalCortex.pkl" \
+  --model /STORAGE/csbig/sc_ADers/celltipy_model_PC.pkl \
   --majority_voting \
   --min_prob 0.6 \
   --out_csv /STORAGE/csbig/sc_ADers/celltypist/unassigned_only_minimal/unassigned_celltypist_predictions.csv
