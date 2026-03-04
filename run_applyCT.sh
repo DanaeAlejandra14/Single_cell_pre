b9fb05fa84f43f4e9c4a14d7d011391c34816595
Danae Alejandra Cortes García <alejandradanae14@gmail.com>
2026-03-04 16:04:36 -0600

    Create run_applyCT.sh

---
+++ run_applyCT.sh
@@ -0,0 +1,6 @@
+Rscript ~/Single_cell_pre/apply_predictions_CT.R \
+  --seurat_rds /STORAGE/csbig/sc_ADers/out_annotated_unassigned/merged_harmony_integrated_annotated.rds \
+  --pred_csv /STORAGE/csbig/sc_ADers/celltypist/unassigned_only_minimal_2/unassigned_celltypist_predictions.csv \
+  --out_dir /STORAGE/csbig/sc_ADers/celltypist/unassigned_only_minimal_2 \
+  -w 12 \
+  --seed 42
