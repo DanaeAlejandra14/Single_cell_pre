50c3840a2b13fa19414cb731920ecea2e6f13b2e
Danae Alejandra Cortes García <alejandradanae14@gmail.com>
2026-03-10 16:52:22 -0600

    Update run_selectNA.sh

--- run_selectNA.sh
+++ run_selectNA.sh
@@ -1,5 +1,8 @@
 Rscript ~/Single_cell_pre/select_na.R \
-  --seurat_rds /STORAGE/csbig/sc_ADers/out_annotated_unassigned/merged_harmony_integrated_annotated.rds \
-  --out_dir /STORAGE/csbig/sc_ADers/celltypist/unassigned_only_minimal_2 \
+  --seurat_rds /STORAGE/csbig/sc_ADers/out_annotated_final/merged_harmony_integrated_annotated.rds \
+  --out_dir /STORAGE/csbig/sc_ADers/celltypist/unassigned_only_final \
   -w 12 \
   --seed 42
+
+
+
