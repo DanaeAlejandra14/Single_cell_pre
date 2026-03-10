7ce6455a9bf161a8f02abcc2703c53397b32b3f1
Danae Alejandra Cortes García <alejandradanae14@gmail.com>
2026-03-10 16:09:50 -0600

    Update run_CT.sh

--- run_CT.sh
+++ run_CT.sh
@@ -1,7 +1,7 @@
 
 python ~/Single_cell_pre/Celltypi_anno.py \
-  --base /STORAGE/csbig/sc_ADers/celltypist/unassigned_only_minimal \
+  --base /STORAGE/csbig/sc_ADers/celltypist/unassigned_only_final \
   --model /STORAGE/csbig/sc_ADers/celltipy_model_PC.pkl \
   --majority_voting \
   --min_prob 0.6 \
-  --out_csv /STORAGE/csbig/sc_ADers/celltypist/unassigned_only_minimal_2/unassigned_celltypist_predictions.csv
+  --out_csv /STORAGE/csbig/sc_ADers/celltypist/unassigned_only_final/unassigned_celltypist_predictions.csv
